c6bea20621f3fe67e359991db2f08cd05ef8fe5d
Ankit Thapa <145375362+Ankithapa08@users.noreply.github.com>
2026-01-15 02:39:42 +0530

    Enhance cookie options with sameSite attribute
    Added 'sameSite' attribute to cookie options for better security.

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -107,9 +107,10 @@ const loginUser = asyncHandler(async(req,res) =>{
         
        const options = {
         httpOnly : true,
-        secure :true
+        secure :true,
+        sameSite: "none",     
        }
-       
+
        return res.status(200).cookie("accessToken",accessToken,options)
        .cookie("refreshToken",refreshToken,options)
        .json(
